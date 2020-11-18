1c45d4c35c38c629caed6463c5bc22be1fd305f4
Markus Felten <Markus.Felten@gmx.de>
2020-11-18 18:12:59 +0100

    add missing tests/travis_setup_arangodb.sh from template

---
+++ tests/travis_setup_arangodb.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "Start setup ArangoDB"
+
+
+docker pull arangodb/arangodb:latest
+docker run -d -e ARANGO_ROOT_PASSWORD="root" -p 8529:8529 arangodb/arangodb:latest
+
+sleep 2
+
+n=0
+# timeout value for startup
+timeout=60
+while [[ (-z `curl -H 'Authorization: Basic cm9vdDpyb290' -s 'http://127.0.0.1:8529/_api/version' `) && (n -lt timeout) ]] ; do
+  echo -n "."
+  sleep 1s
+  n=$[$n+1]
+done
+
+if [[ n -eq timeout ]];
+then
+    echo "Could not start ArangoDB. Timeout reached."
+    exit 1
+fi
+
+echo "ArangoDB is up"
+
+node ./tests/setupLogDB
+
+sleep 6
+
+echo "Arango LOG database created"
